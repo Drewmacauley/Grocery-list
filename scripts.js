e056815bcf79436dad59864b44ee4cb050b6d427
Drewmacauley <macauleyandy03@gmail.com>
2020-08-17 15:45:03 -0700

    add html,some script

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+$("form#grocerylist").submit(function(event) {
+  event.preventDefault();
+    // const itemData = $("input.item").val();
+  console.log($("input.item"))
+  let itemArray = []
+  $("input.item").each(function (Item) {
+
+  }
+
+
+
+  })
+});
